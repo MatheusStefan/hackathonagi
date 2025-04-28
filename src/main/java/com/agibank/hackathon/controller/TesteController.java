c723e18ecd011805b4881a460778bbcc80843ce7
matheus.rocco <roccomatheus0@gmail.com>
2025-04-28 12:23:44 -0300

    estrutura base

---
+++ src/main/java/com/agibank/hackathon/controller/TesteController.java
@@ -0,0 +1,15 @@
+package com.agibank.hackathon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TesteController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "Teste";
+    }
+}
